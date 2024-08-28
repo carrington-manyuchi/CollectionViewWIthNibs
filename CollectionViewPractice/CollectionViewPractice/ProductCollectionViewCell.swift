0c69ade27ff2c425d6ca51f05bc34ff3e32a7566
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-28 10:48:34 +0200

    cell

---
+++ CollectionViewPractice/CollectionViewPractice/ProductCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.swift
+//  CollectionViewPractice
+//
+//  Created by Manyuchi, Carrington C on 2024/08/28.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CollectionViewPractice/CollectionViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewPractice
+//
+//  Created by Manyuchi, Carrington C on 2024/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
